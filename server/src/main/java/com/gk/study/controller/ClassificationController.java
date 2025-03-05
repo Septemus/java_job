c4a772039eb1486bade5006115141efb0d824750
joe <musketeerdt@gmail.com>
2025-03-05 21:07:47 +0800

    create classfication api use json

--- server/src/main/java/com/gk/study/controller/ClassificationController.java
+++ server/src/main/java/com/gk/study/controller/ClassificationController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,9 +37,9 @@ public class ClassificationController {
     }
 
     @Access(level = AccessLevel.ADMIN)
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @RequestMapping(value = "/create", method = RequestMethod.POST,consumes = {"application/json"})
     @Transactional
-    public APIResponse create(Classification classification) throws IOException {
+    public APIResponse create(@RequestBody Classification classification) throws IOException {
         service.createClassification(classification);
         return new APIResponse(ResponeCode.SUCCESS, "创建成功");
     }
